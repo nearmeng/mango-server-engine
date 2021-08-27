2a02c504dc2dd2668db4625cd88107f435520c16
stevhuang <stevhuang@tencent.com>
2021-08-27 11:52:44 +0800

    shm modify

---
+++ src/test/testing/linked_list_test.cpp
@@ -0,0 +1,40 @@
+#include "gtest.h"
+#include "shm/shm_pool.h"
+#include "linked_list/linked_list.h"
+
+struct SHM_TEST_DATA : LINK_NODE
+{
+    int a;
+    char szName[16];
+};
+
+TEST(LIST_TEST, COMMON_TEST)
+{
+	int32_t nRetCode = 0;
+	LINK_HEAD Head = { 0 };
+
+    nRetCode = CShmMgr::instance().init(0x1234, 10 * 1024 * 1024, false);
+    ASSERT_TRUE(nRetCode);
+
+    CShmObjectPool<SHM_TEST_DATA, int32_t> TestDataPool;
+    nRetCode = TestDataPool.init(1, 10, false);
+    ASSERT_TRUE(nRetCode);
+
+    SHM_TEST_DATA* pData1 = TestDataPool.new_object(123);
+    ASSERT_TRUE(pData1 != NULL);
+    
+	SHM_TEST_DATA* pData2 = TestDataPool.new_object(456);
+    ASSERT_TRUE(pData2 != NULL);
+
+	nRetCode = add_tail(&Head, CShmMgr::instance().ptr2mid(pData1));
+	ASSERT_TRUE(nRetCode);
+
+	nRetCode = add_tail(&Head, CShmMgr::instance().ptr2mid(pData2));
+	ASSERT_TRUE(nRetCode);
+
+	nRetCode = del_node(&Head, CShmMgr::instance().ptr2mid(pData1));
+	ASSERT_TRUE(nRetCode);
+	
+	nRetCode = del_node(&Head, CShmMgr::instance().ptr2mid(pData2));
+	ASSERT_TRUE(nRetCode);
+}
